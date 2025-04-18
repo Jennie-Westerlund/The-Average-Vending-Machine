faf69e31e149b9b630e374f23e425b01fc585129
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-04-18 21:53:58 +0200

    added products and functions I'll want in inventory

--- The-Average-Vending-Machine/The-Average-Vending-Machine/AverageInventory.cs
+++ The-Average-Vending-Machine/The-Average-Vending-Machine/AverageInventory.cs
@@ -2,12 +2,41 @@ namespace The_Average_Vending_Machine;
 
 public class AverageInventory
 {
-    products = new List<Product>
+    public List<Product> Products { get; private set; }
+
+    public AverageInventory()
     {
-        new Product("A1" "Snickers", 1.50m),
-        new Product("A2" M&Ms", 1.25m),
-        new Product("Chips", 2.00m),
-        new Product("Water", 1.00m),
-        new Product("Soda", 1.75m)
+        InitializeInventory();
+    }
+
+    private void InitializeInventory()
+    {
+        Products = new List<Product>
+        {
+            new Product("Snickers", 10, "A1", 10),
+            new Product("Twix", 11, "A2", 10),
+            new Product("Maltesers", 18, "A3", 10),
+            new Product("Kryptoniter", 15, "B1", 10),
+            new Product("Turkisk peppar", 10, "B2", 10),
+            new Product("Salta grodor", 12, "B3", 10),
+            new Product("Sura bilar", 20, "C1", 10),
+            new Product("Djungelvrål", 14, "C2", 10),
+            new Product("Hallon skallar", 15, "C3", 10),
+        };
+    }
+
+    public void DisplayInventory()
+    {
+        //code for showing current products in inventory
+    }
+
+    public Product? GetProductByPlacement(string placement)
+    {
+        //get the product the user wanted to by?
+    }
+
+    public bool PurchaseProduct(string placement)
+    {
+        //logic for uppdating inventory when user buys product
     }
 }
