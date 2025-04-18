c3e417735526a56b288a97e6c746933c3c64885f
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-04-18 21:21:37 +0200

    added logic for the bank

--- The-Average-Vending-Machine/The-Average-Vending-Machine/TheAverageBank.cs
+++ The-Average-Vending-Machine/The-Average-Vending-Machine/TheAverageBank.cs
@@ -2,5 +2,25 @@ namespace The_Average_Vending_Machine;
 
 public class TheAverageBank
 {
-    
+    private User? _currentUser;
+
+    public void SetUser(User user)
+    {
+        _currentUser = user;
+    }
+
+    public decimal GetWalletAmount()
+    {
+        //wallet amount from user input
+    }
+
+    public bool ProcessPayment(decimal amount)
+    {
+        //fuction to uppdate amount in wallet
+    }
+
+    public bool IsBroke()
+    {
+        //function to determine broke-status
+    }
 }
