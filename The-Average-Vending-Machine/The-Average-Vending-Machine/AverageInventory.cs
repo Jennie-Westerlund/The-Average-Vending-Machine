bf5942895327828f63aa1003a589ad2efe640028
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-04-18 21:13:50 +0200

    added user

--- The-Average-Vending-Machine/The-Average-Vending-Machine/AverageInventory.cs
+++ The-Average-Vending-Machine/The-Average-Vending-Machine/AverageInventory.cs
@@ -2,5 +2,12 @@ namespace The_Average_Vending_Machine;
 
 public class AverageInventory
 {
-    
+    products = new List<Product>
+    {
+        new Product("A1" "Snickers", 1.50m),
+        new Product("A2" M&Ms", 1.25m),
+        new Product("Chips", 2.00m),
+        new Product("Water", 1.00m),
+        new Product("Soda", 1.75m)
+    }
 }
--- The-Average-Vending-Machine/The-Average-Vending-Machine/User.cs
+++ The-Average-Vending-Machine/The-Average-Vending-Machine/User.cs
@@ -1,6 +1,23 @@
 namespace The_Average_Vending_Machine;
 
-class User
+public class User
 {
-    
+    public string Name { get; set; }
+    public decimal WalletAmount { get; set; }
+
+    public User(string name, decimal walletAmount)
+    {
+        Name = name;
+        WalletAmount = walletAmount;
+    }
+
+    public bool CanAfford(decimal price)
+    {
+        return WalletAmount >= price;
+    }
+
+    public void DeductMoney(decimal amount)
+    {
+        WalletAmount -= amount;
+    }
 }
