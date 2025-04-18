6a34a10a8803b56bf0ee873cacc35f7147dce571
Jennie-Westerlund <jmn.westerlund@gmail.com>
2025-04-18 21:37:26 +0200

    added basic product-values

--- The-Average-Vending-Machine/The-Average-Vending-Machine/Product.cs
+++ The-Average-Vending-Machine/The-Average-Vending-Machine/Product.cs
@@ -2,5 +2,16 @@ namespace The_Average_Vending_Machine;
 
 public class Product
 {
-    
+    public string Name { get; }
+    public decimal Price { get; }
+    public string Placement { get; }
+    public int Quantity { get; set; }
+
+    public Product(string name, decimal price, string placement, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Placement = placement;
+        Quantity = quantity;
+    }
 }
